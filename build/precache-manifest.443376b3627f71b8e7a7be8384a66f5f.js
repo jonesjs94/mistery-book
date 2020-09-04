a7ca9954886b81ae65616f1bc69ccef032dcab62
Jonathan <jonathan_guerreiro@hotmail.com>
2020-09-04 14:11:41 -0300

    build

---
+++ build/precache-manifest.443376b3627f71b8e7a7be8384a66f5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4f55daa9474dc870f3e729d69e0c8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71882107bf70a169ad99",
+    "url": "/static/css/2.adbb5aae.chunk.css"
+  },
+  {
+    "revision": "4d629e8cf7c85d8fae83",
+    "url": "/static/css/main.1e8581d7.chunk.css"
+  },
+  {
+    "revision": "71882107bf70a169ad99",
+    "url": "/static/js/2.482eb5e7.chunk.js"
+  },
+  {
+    "revision": "fd05a38f8e0105a6a30010277bee16a1",
+    "url": "/static/js/2.482eb5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d629e8cf7c85d8fae83",
+    "url": "/static/js/main.c48f447e.chunk.js"
+  },
+  {
+    "revision": "a67d35e409acf93956aa",
+    "url": "/static/js/runtime-main.e92dfcc8.js"
+  },
+  {
+    "revision": "f9a2228d683b138967180ff11697a9db",
+    "url": "/static/media/all.f9a2228d.svg"
+  }
+]);
